14db493b1c40fa7b37ab84c3b65e5e0f0bd7526a
Camilo Espinosa <espinosacamilop1@gmail.com>
2021-10-26 22:42:50 -0400

    set up all phone brand linksand added a model to the iphone link, work to be continued

--- src/App.js
+++ src/App.js
@@ -2,23 +2,45 @@ import React from "react";
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import Home from "./Components/Home";
+import Admin from "./Components/Admin";
+import NavBar from "./Components/NavBar";
+import IPhone from "./Components/Phones/iPhone";
+
 function App() {
   return (
     <div className="App">
-      {
+     
         <Router>
+          <NavBar/>
             <Switch>
               <Route path="/" exact>
-                This is home
+                <Home/>
               </Route>
               <Route path="/admin">
-                This is admin route
+                <Admin/>
+              </Route>
+              <Route path='/iphone'>
+                    <IPhone/>
+              </Route>
+              <Route path='/samsung'>
+                    this is all samsung models we buy
+              </Route>
+              <Route path='/pixel'>
+                this is all pixel models we buy
+              </Route>
+              <Route path='/ipad'>
+                this is all ipad models we buy
+              </Route>
+              <Route path='/macbook'>
+                this is all macbook models we buy
               </Route>
             </Switch>
         </Router>
-      }
+      
     </div>
   );
 }
+
 
 export default App;
