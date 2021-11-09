9364a831b75d401b6e45e2421fcbefa2b3d200f9
Mac <ricardofer2412@hotmail.com>
2021-11-09 17:30:00 -0500

    add table

--- src/Components/Admin/Users/Users.js
+++ src/Components/Admin/Users/Users.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 export default class Users extends Component {
   state = {
     usersList: [],
@@ -23,16 +31,47 @@ export default class Users extends Component {
   render() {
     return (
       <div>
-        This show all the users
-        <Link to="/admin/createuser">Create Users</Link>
+         <Link to="/admin/createuser">Create Users</Link>
+         <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">User Id</TableCell>
+            <TableCell align="right">Username</TableCell>
+            <TableCell align="right">Role</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Status</TableCell>
+      
+          </TableRow>
+        </TableHead>
+        <TableBody>
         {this.state.usersList.map((user) => (
-          <div>
-            <p>{user._id}</p>
-            <p>{user.username}</p>
-            <p>{user.role}</p>
-            <p>{user.userEmail}</p>
-          </div>
-        ))}
+            <TableRow
+              key={user._id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{user._id}</TableCell>
+              <TableCell align="right">{user.username}</TableCell>
+              <TableCell align="right">{user.role}</TableCell>
+              <TableCell align="right">{user.userEmail}</TableCell>
+              {user.active === true ?  (
+            <TableCell align="right">Active</TableCell>
+            ) : (
+              <div>
+             <TableCell align="right">Inactive</TableCell>
+              </div>
+            )
+  }
+            
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+       
+ 
+        
       </div>
     );
   }
