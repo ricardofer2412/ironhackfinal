b3aeba8b7d81a867619f814c00baeb8b57437e28
ricardofer2412 <richardcasanovac@gmail.com>
2021-11-20 01:05:48 -0500

    Update mongo connection url

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/backend";
+// const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/backend";
+const MONGO_URI =
+  "mongodb+srv://lossuperdevs:MNeeW29MjBwYwXv@cluster0.exzbs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI)
